1f6c01205fb0dc2f1c8de64ba79ab40c6887574d
Dali-debug <mohamedali.bensalah@supcom.tn>
2025-04-13 14:05:09 +0100

    supprimer

--- run_auth.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-g++ main.cpp user.cpp file_handler.cpp hash.cpp -lssl -lcrypto -o auth_system
-if %ERRORLEVEL% NEQ 0 (
-    echo Compilation failed.
-    pause
-    exit /b %ERRORLEVEL%
-)
-auth_system.exe
-pause
